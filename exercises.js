d7847988f670f79021f3727813c545535edb2dc3
Mariano Montrasi <marianomontrasi@gmail.com>
2020-10-26 16:57:18 -0300

    TP-04: 1.b

--- exercises.js
+++ exercises.js
@@ -1,7 +1,12 @@
 /* Practice Number One*/
 /*1.a*/
 var num1, num2;
+num1 = 5;
+num2 = 10;
 var suma1y2= num1 + num2;
+console.log(suma1y2)
 
-console.log(suma1y2);
-
+/*1.b*/
+var nombre ='Mariano', apellido = 'Montrasi';
+var nombreCompleto = nombre + apellido;
+console.log(nombreCompleto)
