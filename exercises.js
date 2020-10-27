b1d00eccc8ec9ee472b3765dca578dc56a63cb29
Mariano Montrasi <marianomontrasi@gmail.com>
2020-10-27 14:38:03 -0300

    TP-04: 4.b

--- exercises.js
+++ exercises.js
@@ -83,4 +83,22 @@ if (numeroAleatorio>=0.5) {
                         console.log('Greater than 0,5');
                             } else {
                                     console.log('Lower than 0,5');
-                                    }
+}
+
+/*4.b*/
+var age = 60;
+if (age<2) {
+    console.log('Bebe');
+            }else if(age>=2 && age<=12) {
+                console.log('Nino');
+            }else if(age>=13 && age<=19) {
+                console.log('Adolescente');
+            }else if(age>=20 && age<=30) {
+                console.log('Joven');
+            }else if(age>=31 && age<=60) {
+                console.log('Adulto');
+            }else if(age>=61 && age<=75) {
+                console.log('Adulto Mayor');
+            }else {
+                console.log('Anciano');
+}        
